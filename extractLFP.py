ca59676cf0ec10c281e04a550bacf79fcc3f29fe
David Tingley <davidtingley2@gmail.com>
2017-03-09 00:16:00 -0500

    extractLFP1.0 seems to be working

--- extractLFP.py
+++ extractLFP.py
@@ -2,29 +2,52 @@
 
 import numpy as np
 import scipy as sp
+import scipy.signal as sig
+import argparse
+import os
+import matplotlib.pyplot as plt
 
+def main(args):
+    # check here and remove/rename old lfp files?
 
-def main():
+    # start
     dataFolder = args.dataFolder
     inputFile = args.inputFile
     outputFile = args.outputFile
     nChannels = args.nChannels
 
     chunkSize = 20000
-    fid = open([dataFolder + '/' + inputFile, 'r'])
-    out = open([dataFolder + '/' + outputFile, 'w'])
+    inputFile = dataFolder + '/' + inputFile
+    recordingSize = float(os.path.getsize(inputFile))
 
+    fid = open(inputFile, 'r')
+    outputFile = dataFolder + '/' + outputFile
+    out = open(outputFile, 'w')
 
     a = np.fromfile(fid, count=chunkSize*nChannels, dtype=np.int16)
-
-    lfp = a.reshape(nChannels,chunkSize)
-    lfp_down = np.ndarray((nChannels,1250))
-    for i in range(0,nChannels,1):
-        lfp_down[i,:] = sp.signal.resample(lfp[i,:],1250)
-
-    b = lfp_down.reshape(nChannels*chunkSize,1)
-    b.tofile(out)
-    
+    counter = 0;
+    while a.size > 1:
+        lfp = a.reshape(nChannels,chunkSize,order='F')
+        lfp_down = np.ndarray((nChannels,1250))
+        # plt.subplot(211)
+        # plt.plot(np.transpose(lfp))
+        for i in range(0,nChannels,1):
+            lfp_down[i,:] = sig.resample(lfp[i,:],1250)
+        #     plt.subplot(212)
+        #     plt.plot(np.transpose(lfp_down))
+        # plt.show()
+        b = lfp_down.reshape(nChannels*1250,1,order='F')
+        # plt.subplot(211)
+        # plt.plot(b[1:1250])
+        # plt.subplot(212)
+        # plt.plot(a[1:20000])
+        # plt.show()
+        # out.write(np.transpose(np.int16(b)))
+        np.transpose(np.int16(b)).tofile(out)
+        a = np.fromfile(fid, count=chunkSize*nChannels, dtype=np.int16)
+        if counter % 60 == 0:
+            print(['extracting LFP: ' + str((counter*chunkSize*nChannels)/recordingSize) + ' percent done...'])
+        counter = counter + 1
 
 
 if __name__ == "__main__":
@@ -33,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('dataFolder', type=str, default=os.getcwd(), help='the folder with all of your recordings in subdirectories')
     parser.add_argument('nChannels', type=np.int16, default=1, help='the number of channels')
     parser.add_argument('inputFile', type=str, default='amplifier.dat', help='input .dat file')
-    parser.add_argument('-outputFile', type=str, default='amplifier.dat', help='output .lfp file [default = inputFile.lfp]')
+    parser.add_argument('-outputFile', type=str, default='amplifier.lfp', help='output .lfp file [default = inputFile.lfp]')
     # parser.add_argument('-numJobs',type=float,default=4,help='number of jobs to run simultaneously [default = 4]')
     # parser.add_argument('-cpuLimit',type=float,default=80,help='cpu usage limit [default = 80]')
     # parser.add_argument('-repoPath',type=str,default=sys.argv[0],help='location of ephys-processing repository')
