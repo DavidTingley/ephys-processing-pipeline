3918cf0818f81be9bfed30168e5ea246efffdf01
David Tingley <davidtingley2@gmail.com>
2017-03-09 19:10:55 -0500

    added options to processRecordings

--- extractLFP.py
+++ extractLFP.py
@@ -1,30 +1,9 @@
 #!/home/david/miniconda/envs/klusta/bin/python
 
 import numpy as np
-import scipy as sp
 import scipy.signal as sig
 import argparse
 import os
-import matplotlib.pyplot as plt
-
-# def sinc_interp(x, s, u):
-#     """
-#     Interpolates x, sampled at "s" instants
-#     Output y is sampled at "u" instants ("u" for "upsampled")
-    
-#     from Matlab:
-#     http://phaseportrait.blogspot.com/2008/06/sinc-interpolation-in-matlab.html        
-#     """
-    
-#     if len(x) != len(s):
-#         raise Exception, 'x and s must be the same length'
-    
-#     # Find the period    
-#     T = s[1] - s[0]
-    
-#     sincM = np.tile(u, (len(s), 1)) - np.tile(s[:, np.newaxis], (1, len(u)))
-#     y = np.dot(x, np.sinc(sincM/T))
-#     return y
 
 def main(args):
     # check here and remove/rename old lfp files?
@@ -34,10 +13,11 @@ def main(args):
     inputFile = args.inputFile
     outputFile = args.outputFile
     nChannels = args.nChannels
-
-    chunkSize = 20000
+    chunkSize = args.recSamplingRate
+    lfpChunk = args.lfpSamplingRate
+    
     inputFile = dataFolder + '/' + inputFile
-    recordingSize = float(os.path.getsize(inputFile))
+    recordingSize = int(os.path.getsize(inputFile))
 
     fid = open(inputFile, 'r')
     outputFile = dataFolder + '/' + outputFile
@@ -48,27 +28,24 @@ def main(args):
     fWindow = sig.get_window('hamming',1)
 
     while a.size > 1:
-        lfp = a.reshape(nChannels,chunkSize,order='F')
-        lfp_down = np.ndarray((nChannels,1250))
-        # plt.subplot(211)
-        # plt.plot(np.transpose(lfp))
+        try: # if len(a) == chunkSize*nChannels?
+            lfp = a.reshape(nChannels,chunkSize,order='F')
+        except: # elseif len(a) < chunkSize*nChannels?
+            chunkSize = a.size / nChannels # re-assign chunkSize for the last chunk of data
+            lfpChunk = chunkSize / 16
+            print(chunkSize, lfpChunk)
+            lfp = a.reshape(nChannels,chunkSize,order='F')
+            
+        lfp_down = np.ndarray((nChannels,lfpChunk))
+
         for i in range(0,nChannels,1):
-            lfp_down[i,:] = sig.resample_poly(lfp[i,:],1250,chunkSize,window=fWindow)
-            # lfp_down[i,:] = sinc_interp(lfp[i,:],np.arange(0,1,1/20000.),np.arange(0,1,1/1250.))
-        #     plt.subplot(212)
-        #     plt.plot(np.transpose(lfp_down))
-        # plt.show()
-        b = lfp_down.reshape(nChannels*1250,1,order='F')
-        # plt.subplot(211)
-        # plt.plot(b[1:1250])
-        # plt.subplot(212)
-        # plt.plot(a[1:20000])
-        # plt.show()
-        # out.write(np.transpose(np.int16(b)))
+            lfp_down[i,:] = sig.resample_poly(lfp[i,:],lfpChunk,chunkSize,window=fWindow)
+
+        b = lfp_down.reshape(nChannels*lfpChunk,1,order='F')
         np.transpose(np.int16(b)).tofile(out)
         a = np.fromfile(fid, count=chunkSize*nChannels, dtype=np.int16)
-        if counter % 60 == 0:
-            print(['extracting LFP: ' + str((counter*chunkSize*nChannels)/recordingSize) + ' percent done...'])
+        if counter % 360 == 0:
+            print(['extracting LFP: ' + str(200*(counter*chunkSize*nChannels)/recordingSize) + ' percent done...'])
         counter = counter + 1
 
 
@@ -79,10 +56,9 @@ if __name__ == "__main__":
     parser.add_argument('nChannels', type=np.int16, default=1, help='the number of channels')
     parser.add_argument('inputFile', type=str, default='amplifier.dat', help='input .dat file')
     parser.add_argument('-outputFile', type=str, default='amplifier.lfp', help='output .lfp file [default = inputFile.lfp]')
-    # parser.add_argument('-numJobs',type=float,default=4,help='number of jobs to run simultaneously [default = 4]')
-    # parser.add_argument('-cpuLimit',type=float,default=80,help='cpu usage limit [default = 80]')
-    # parser.add_argument('-repoPath',type=str,default=sys.argv[0],help='location of ephys-processing repository')
+    parser.add_argument('-recSamplingRate', type=int, default=20000, help='sampling rate of the recording [default = 20000]')
+    parser.add_argument('-lfpSamplingRate', type=int, default=1250, help='desired sampling rate of LFP file [default = 1250]')
+    
     args = parser.parse_args()
-    print(args)
     main(args)
 
