0d0d5343e2ea1d7e113af0c635023e4689f1cc53
David Tingley <davidtingley2@gmail.com>
2017-10-02 19:09:42 -0400

    /

--- probemap.m
+++ probemap.m
@@ -2,6 +2,7 @@ function [] = probemap(DATAFOLDER,RECORDING,SHANK)
 
 addpath(genpath('/ifs/home/dwt244/buzcode'));
 addpath(genpath('/ifs/home/dwt244/ephys-processing-pipeline'));
-makeProbeMap_phoenix(DATAFOLDER,RECORDING,SHANK);
+% makeProbeMap_phoenix(DATAFOLDER,RECORDING,SHANK);
+makeProbeMap(DATAFOLDER,RECORDING);
 
 end
