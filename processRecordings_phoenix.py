2351e30bfcaa8d14d26969a91bc7cebebcce1205
David Tingley <davidtingley2@gmail.com>
2017-10-02 19:16:08 -0400

    switch to pre-phoenix version

--- processRecordings_phoenix.py
+++ processRecordings_phoenix.py
@@ -126,26 +126,24 @@ def checkJobLimits(cpuLimit, numJobs, waitTime):
 
 
 def checkShankDirsExist(root,subdirList, dirName, numShanks, xmlfile,repoPath):
-    # try:
-    subdirList = [d for d in subdirList if not '201' in d if not
-                  'extras' in d if not 'temp' in d if not 'Session' 
-                  in d if not 'State' in d]  # removes folders that are not shank folders
-    if len(subdirList) < numShanks:
-        # this section needs to be abtracted to the number of
-        # shanks instead of a hard number...
-        recording = xmlfile.split('.')[0]
-        print(os.path.abspath(dirName))
-        for sh in range(1,int(numShanks),1):
-            matlab_command = ['qsub ~/run_matlab_test.bash "addpath(genpath(\'' + repoPath + '\')); probemap(\'' + \
-            root + ',' + recording + '\',' + str(sh) +')"']
+    try:
+        subdirList = [d for d in subdirList if not '201' in d if not
+                      'extras' in d if not 'temp' in d if not 'Session' 
+                      in d if not 'State' in d]  # removes folders that are not shank folders
+        if len(subdirList) < numShanks:
+            # this section needs to be abtracted to the number of
+            # shanks instead of a hard number...
+            print(os.path.abspath(dirName))
+            matlab_command = ['matlab -nodesktop -r "addpath(genpath(\'' + repoPath + '\'));  \
+                makeProbeMap(\'' + os.path.abspath(dirName) + '\',\'' + xmlfile[0] + '\');exit"']
             # generate folder structure and .prm/.prb files
             print(matlab_command)
             subprocess.call(matlab_command[0], shell=True)
-        time.sleep(10)  # let the process get going...
-    return True
-# except:
-    print('errorrr')
-    return False
+            time.sleep(10)  # let the process get going...
+        return True
+    except:
+        print('errorrr')
+        return False
 
 
 def extractBehaviorTracking(xmlfile):
